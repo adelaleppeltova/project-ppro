476f16233b9db7c94443e6fe39bca7932415ece9
Adéla Leppeltová <83660219+adelaleppeltova@users.noreply.github.com>
2024-01-14 18:19:44 +0100

    init

---
+++ src/test/java/com/example/projectppro/ProjectPproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectppro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
